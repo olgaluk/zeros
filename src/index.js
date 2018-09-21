18788fbde193bd2f6fddd7c0e45577806247ae5c
Olga Luk <olga_ivanova_86@mail.ru>
2018-09-21 13:31:19 +0300

    zero count

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  var zeros = 0;
+  while (number>0) {
+   number = ~~(number/5);
+   zeros = zeros + number;
+  }
+  return zeros;
 }
